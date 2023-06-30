bb24c7a4aa61b73d8ee3aa117ffda94d75673bfe
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2023-07-01 00:19:32 +0900

    FIX : api 수정에 따른 response 가져오는 방식 수정

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import os
 import requests
 import base64
 import qrcode
@@ -7,17 +6,17 @@ import time
 while True:
     try:
         response = requests.get('http://127.0.0.1:5001/api/random')
-        random_string = response.text.strip()
-        
+        random_string = response.json()["generateNum"]
+
         base64_encoded = base64.b64encode(random_string.encode()).decode()
-        
+
         qr = qrcode.QRCode()
         qr.add_data(base64_encoded)
         qr.make(fit=True)
-        
+
         img = qr.make_image(fill='black', back_color='white')
         img.save('qrcode.png')
-        
+
         time.sleep(3)  # 3초간 대기
     except KeyboardInterrupt:
         break  # Ctrl+C를 누르면 반복 종료
